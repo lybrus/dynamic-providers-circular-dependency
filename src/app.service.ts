7198cb1f87fe1ba47fda43b10dcf104dfe3df10e
Damir Rashidov <damir@rashidov.eu>
2023-02-10 06:59:41 +0200

    Add timeouts
    Signed-off-by: Damir Rashidov <damir@rashidov.eu>

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,7 +10,9 @@ export class AppService {
 export const dynamicProvider1 = {
   provide: 'PROVIDER1',
   useFactory: (provider2: any) => {
-    console.log('from provider 1', provider2);
+    setTimeout(() => {
+      console.log('from provider 1', provider2);
+    }, 1);
     return {
       name: 'provider 1',
     };
@@ -21,7 +23,9 @@ export const dynamicProvider1 = {
 export const dynamicProvider2 = {
   provide: 'PROVIDER2',
   useFactory: (provider1: any) => {
-    console.log('from provider 2', provider1);
+    setTimeout(() => {
+      console.log('from provider 2', provider1);
+    }, 1);
     return {
       name: 'provider 2',
     };
